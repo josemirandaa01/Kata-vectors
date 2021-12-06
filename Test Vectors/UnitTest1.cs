2f0446a215323188517695a384015e40b9ad2785
jose <josemirandaalmeyda@hotmail.com>
2021-12-06 19:04:28 -0400

    version 1.0

--- Test Vectors/UnitTest1.cs
+++ Test Vectors/UnitTest1.cs
@@ -1,18 +1,31 @@
 using NUnit.Framework;
+using VectorLibrary;
 
 namespace Test_Vectors
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
+
+        [Test]
+        public void Vector_Test()
         {
+           int valueX = 1;
+           int valueY = 5;
+           (double, double) expected = (1,5);
+           (double, double) answer = VectorLibrary.Vectores.Vector(valueX,valueY);
+
+            Assert.AreEqual(expected,answer);
         }
 
         [Test]
-        public void Test1()
+        public void Magnitude_Test()
         {
-            Assert.Pass();
+            double x1 = 1, x2 = 5, y1 = 1, y2 = 3;
+            double expected = 4.4721359549995796;
+            double answer = VectorLibrary.Vectores.Magnitude(x1,x2,y1,y2);
+
+            Assert.AreEqual(expected, answer);
         }
+
     }
 }
---
+++ VectorLibrary/Vectores.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace VectorLibrary
+{
+    public class Vectores
+    {
+        public double x;
+        public double y;
+
+        public static (double,double) Vector(int x, int y)
+        {
+            Vectores vector = new Vectores();
+            vector.x = x;
+            vector.y = y;
+
+            return (vector.x, vector.y);
+        }
+
+        public static double Magnitude(double x1,double x2,double y1,double y2)
+        {
+            double answer = Math.Sqrt(Math.Pow((x2 - x1),2) + Math.Pow((y2 - y1),2));
+            return answer;
+        }
+    }
+
+
+}
