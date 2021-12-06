1384c69edb726f36551cc9d56ed16c25653804c0
jose <josemirandaalmeyda@hotmail.com>
2021-12-06 18:22:12 -0400

    inicio

---
+++ Test Vectors/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Test_Vectors
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
